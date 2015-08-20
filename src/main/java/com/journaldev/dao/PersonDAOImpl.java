faed11009ff79ffcc492a0c26dc36830447da05d
Priyanka_Sood01 <Priyanka_Sood01@BLRKEC104038L.ad.infosys.com>
2015-08-20 21:07:20 +0530

    changes to bind postgresql service-spring.xml,PersonDAOImpl

--- src/main/java/com/journaldev/dao/PersonDAOImpl.java
+++ src/main/java/com/journaldev/dao/PersonDAOImpl.java
@@ -5,17 +5,20 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import main.java.com.journaldev.model.Person;
 import main.java.com.journaldev.util.DBUtil;
 
 
 public class PersonDAOImpl implements PersonDAO {
 
-	private SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
+	/*private  SessionFactory sessionFactory;
 
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-    }
+    }*/
 	private DBUtil dbUtil;
 	
 	public void setDbUtil(DBUtil dbUtil) {
@@ -32,7 +35,9 @@ public class PersonDAOImpl implements PersonDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Session session = this.sessionFactory.openSession(conn);
+		//Session session = this.sessionFactory.openSession(conn);
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession(conn);
 		//Session session = this.sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
 		session.persist(p);
@@ -50,7 +55,9 @@ public class PersonDAOImpl implements PersonDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Session session = this.sessionFactory.openSession(conn);
+		//Session session = this.sessionFactory.openSession(conn);
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession(conn);
 		//Session session = this.sessionFactory.openSession();
 		List<Person> personList = session.createQuery("from Person").list();
 		session.close();
